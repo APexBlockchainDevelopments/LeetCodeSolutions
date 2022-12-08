a1cc546aef71cf0e01386506c58e068fdfb2c663
ZDev-9 <98428148+ZDev-9@users.noreply.github.com>
2022-12-07 22:35:02 -0700

    cs 1929

---
+++ Csharp/1929.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int[] result = new int[nums.Length *2];
+        for(int i = 0; i < nums.Length; i++){
+            result[i] = nums[i];
+            result[i + nums.Length] = nums[i];
+        }
+        return result;
+    }
+}
