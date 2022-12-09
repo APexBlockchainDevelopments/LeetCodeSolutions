b904e1a8cc50457b688e0d11daacb7cdccf93d4b
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2022-12-09 14:01:04 -0700

    1480 js

--- JS/1480.js
+++ JS/1480.js
@@ -4,13 +4,11 @@
  */
 var runningSum = function(nums) {
     let arr = []
+
     for(let i = 0; i < nums.length; i++){
-        if(i == 0){
-            arr[i] = nums[i];
-        } else if(i > 0){
-            arr[i] = nums[i] + arr[i-1];
-        }
+        if(i === 0) arr[i] = nums[i]
+        else arr[i] = nums[i] + arr[i -1]
     }
-    
+
     return arr
 };
