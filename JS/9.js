d42f7d6b43fdb80278d9bef6c7f2af58522ef04e
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2022-12-09 09:25:51 -0700

    js 9

--- JS/9.js
+++ JS/9.js
@@ -2,17 +2,14 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
+var isPalindrome = function (x) {
     let xString = [...x.toString()]
     let xStringReversed = []
 
-    for(let i = 0; i < xString.length; i++){
-       xStringReversed.push(xString[xString.length-i-1]);
+    for (let i = 0; i < xString.length; i++) {
+        xStringReversed.push(xString[xString.length - i - 1]);
     }
 
-    if(xString.toString() === xStringReversed.toString()){
-        return true
-    } else {
-        return false
-    }
+    if (xString.toString() === xStringReversed.toString()) return true
+    else return false
 };
