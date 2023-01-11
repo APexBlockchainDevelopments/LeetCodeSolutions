479bdb5140b9e6d773b99c8f40d862fcd1faf855
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2023-01-11 01:56:09 -0700

    updated rust 2469

--- Rust/2469.rs
+++ Rust/2469.rs
@@ -4,4 +4,34 @@ impl Solution {
         let f = celsius * 1.80 + 32.00;
         return vec![k, f]
     }
+
+    //second way
+    pub fn convert_temperature2(celsius: f64) -> Vec<f64> {
+        let k = celsius + 273.15;
+        let f = celsius * 1.80 + 32.00;
+        let result = vec![k, f];
+        return result
+    }
+
+    ///Third way
+    pub fn convert_temperature3(celsius: f64) -> Vec<f64> {
+        let k = celsius + 273.15;
+        let f = celsius * 1.80 + 32.00;
+
+        let mut result = vec![];
+        result[0] = k;
+        result[1] = f;
+        return result
+    }
+
+    //fourth way
+    pub fn convert_temperature3(celsius: f64) -> Vec<f64> {
+        let k = celsius + 273.15;
+        let f = celsius * 1.80 + 32.00;
+
+        let mut result : Vec<f64>= Vec::new();
+        result[0] = k;
+        result[1] = f;
+        return result
+    }
 }
